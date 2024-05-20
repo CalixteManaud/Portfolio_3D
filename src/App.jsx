50bfd41fed9c0dadcc71bd73852723e89f8048ea
Warthoz <manaud.calixte@etu.univ-amu.fr>
2024-05-20 19:38:11 +0200

    Analytics & Speed Insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import {BrowserRouter} from "react-router-dom";
-import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from "./components";
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from "./components"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import React from "react";
 
 const App = () => {
   return (
       <BrowserRouter>
+          <SpeedInsights />
+          <Analytics  />
           <div className="relative z-0 bg-primary">
               <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
                   <Navbar />
